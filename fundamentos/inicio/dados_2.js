40bd5153bc819a39c7863d9baafd742f38051f16
wikten_Alves <wiktenalves@gmail.com>
2023-01-16 12:41:00 -0300

    Como trabalhar com dados
    explicação sobre variáveis e suas aplicações dentro do JavaScript

---
+++ fundamentos/inicio/dados_2.js
@@ -0,0 +1,17 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(19.90 * 0.6);
+
+let precoComDesconto = preco*(1 - desconto);
+console.log(precoComDesconto);
+
+let nome = "Caderno "; // string (Texto) -> sequência de símbolos e caracteres
+let categoria = "Papelaria";
+console.log("Produto: " + nome 
++ ", Categoria: " + categoria
++ ", Preço: " + preco
++ ", Desconto: ") + desconto;
+
+console.log(1 + 1)
+console.log("1" + 1)

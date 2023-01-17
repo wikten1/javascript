43598b07b8e7dab887cb2d0d47f2ac025e09b33c
wikten_Alves <wiktenalves@gmail.com>
2023-01-17 10:17:46 -0300

    Desafio troca de variáveis feito

--- fundamentos/inicio/desafio_area.js
+++ fundamentos/inicio/desafio_area.js
@@ -1,7 +1,10 @@
+
 const PI = 3.141592;
 const raio = 10;
 let AreaCirc = PI * raio * raio;
 let result = AreaCirc + " M^2";
+
+
 
 console.log("O valore da área é " + result);
 
---
+++ fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,14 @@
+let a = 7;
+let b = 94;
+
+console.log(a);
+console.log(b);
+
+//let temp = a
+//a = b;
+//b = temp;
+
+[a,b] = [b,a]
+
+console.log(a);
+console.log(b);

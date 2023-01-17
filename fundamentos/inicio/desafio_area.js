55a27a2e0c33f0b59712f9ef3252b7d1d0a9dfd9
wikten_Alves <wiktenalves@gmail.com>
2023-01-17 10:03:01 -0300

    Desafio PI feito

---
+++ fundamentos/inicio/desafio_area.js
@@ -0,0 +1,12 @@
+const PI = 3.141592;
+const raio = 10;
+let AreaCirc = PI * raio * raio;
+let result = AreaCirc + " M^2";
+
+console.log("O valore da área é " + result);
+
+AreaCirc = Math.PI * raio * raio;
+console.log("O valore da área é " + result);
+
+
+

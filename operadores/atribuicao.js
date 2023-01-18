118c18f2d6d49f8a24b6cd362537271b06786c49
wikten_Alves <wiktenalves@gmail.com>
2023-01-18 12:20:33 -0300

    Ex operadores de atribuição

---
+++ operadores/atribuicao.js
@@ -0,0 +1,17 @@
+let x = 10;
+x = x + 6;
+x = x - 1;
+console.log(x);
+
+let y = 20;
+y += 10;
+y -= 2;
+y *= 2;
+y /= 4;
+y %= 3;
+console.log(y);
+
+let w = "texto";
+w += ", mais texto";
+w += "!!!"
+console.log(w);

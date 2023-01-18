b310ac97d7341ba081648ccb785482d7d1d77878
wikten_Alves <wiktenalves@gmail.com>
2023-01-18 14:38:09 -0300

    Desafio Atribuição feito

---
+++ operadores/desafio_atribuicao.js
@@ -0,0 +1,9 @@
+let texto = 'Lista De Aprovados\n';
+texto += '===================\n';
+texto += '\n';
+texto += '1. Ana Silva\n';
+texto += '2. Pedro Albuquerque\n';
+texto += '3. Guilherme Pereira\n';
+texto += '4. Rebeca França ';
+
+console.log(texto)

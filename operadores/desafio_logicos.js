0cb0b56dcb5e01a187a7e35de0f44cb58e6c3029
wikten_Alves <wiktenalves@gmail.com>
2023-01-20 11:14:17 -0300

    Desafio operadores lógicos feito

---
+++ operadores/desafio_logicos.js
@@ -0,0 +1,12 @@
+//aritmeticos relacionais e lógicos
+let verdadeira1 = 3 * 3 == 9 && 8 > 2;
+let verdadeira2 = !(3 + 5 > 20);
+
+console.log(verdadeira1);
+console.log(verdadeira2);
+
+let falso1 = 3 * 9 == 32 &&  32 !== 7;
+let falso2 = 9 >= 20 || 3 + 9 == 15
+
+console.log (falso1);
+console.log(falso2)

d28e6c2ba56760c697133e7b8c6abc8993411115
wikten_Alves <wiktenalves@gmail.com>
2023-01-25 19:30:49 -0300

    Teste operadores ternários

---
+++ operadores/desafio_ternario.js
@@ -0,0 +1,6 @@
+const a = 8;
+const b = 4;
+const operacao = '/';
+
+const resultado = operacao === '+' ? a + b : operacao === '-' ? a - b : operacao === '*' ? a * b : a / b;
+console.log(resultado);

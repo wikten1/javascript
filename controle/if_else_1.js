7b7787146f18a7929a540631219ce2d52e60afd3
wikten_Alves <wiktenalves@gmail.com>
2023-02-03 12:03:12 -0300

    ex if e else 01

---
+++ controle/if_else_1.js
@@ -0,0 +1,13 @@
+const verdadeiro = true;
+const falso = false;
+
+
+if(verdadeiro)
+   console.log('Exec...');
+else
+   console.log('Não exec...');
+
+if(falso)
+   console.log('Não exec...');
+else
+   console.log('Exec...');

695aa15091d5eda438f723bfcf1324a2e62a1a69
wikten_Alves <wiktenalves@gmail.com>
2023-02-03 15:18:25 -0300

    desafios if e else feitos

---
+++ controle/desafio_if_els_2.js
@@ -0,0 +1,17 @@
+const nota = 7;
+
+
+  if ((nota >= 9) && (nota <= 10)) 
+     console.log('A');
+  else
+    if ((nota >= 7) && (nota < 9))
+       console.log('B');
+    else
+       if ((nota >= 5) && (nota < 7))
+          console.log('C');
+        else
+           if ((nota >= 4.5) && (nota < 5))
+               console.log('D');
+            else 
+              if ((nota < 4.5) && (nota >= 0) )
+                 console.log('F');

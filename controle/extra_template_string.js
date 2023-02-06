e70216d63891dddcfab210047c7494f38f5d64b5
wikten_Alves <wiktenalves@gmail.com>
2023-02-06 16:47:43 -0300

    ex template string

---
+++ controle/extra_template_string.js
@@ -0,0 +1,8 @@
+const i = 2;
+const j = 4;
+
+console.log("i = " + i + ", j = " + j);
+console.log('i = ' + i + '', j = '' + j);
+console.log(`${1 + 2}`);
+console.log(`i = ${i}, j = ${j}`);
+console.log(`${Math.randon}`);

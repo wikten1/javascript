2eaa888e3740cf6d8cb38f1dd5b72055f55f25e7
wikten_Alves <wiktenalves@gmail.com>
2023-02-07 16:27:04 -0300

    ex switch feito

---
+++ controle/switch_1.js
@@ -0,0 +1,55 @@
+let nota = 12.2;
+
+switch(Math.ceil(nota)) {
+    default:
+         console.log("Nota invalida!")
+         break;
+
+    case 10:
+        console.log("Parabéns!");
+        break;
+    
+    case 9:
+        console.log("Parabéns!");
+        break; 
+    
+    case 8:
+        console.log("Parabéns!");
+        break;
+
+    case 7:
+        console.log("Parabéns!");
+        break;
+
+    case 6:
+        console.log("Recuperação!");
+        break;
+
+    case 5:
+        console.log("Recuperação!");
+        break;
+
+    case 4:
+        console.log("Reprovado!")
+        break;
+        
+    case 3:
+        console.log("Reprovado!")
+        break;
+
+    case 2:
+        console.log("Reprovado!")
+        break;
+    
+    case 1:
+        console.log("Muito Reprovado!")
+        break;
+
+    case 0:
+        console.log("Muito Reprovado!")
+        break;
+}
+
+console.log("Fim!")
+
+//case 10: case 9: case 8:

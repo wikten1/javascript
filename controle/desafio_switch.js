5f87afa7d50d7302f1ccb19d0530440ea448cd7a
wikten_Alves <wiktenalves@gmail.com>
2023-02-07 16:52:29 -0300

    Desafio switch feito

---
+++ controle/desafio_switch.js
@@ -0,0 +1,50 @@
+let nota = "A+";
+
+switch(nota) {
+    case "A+":
+        console.log("A+ -> 10")
+        break;
+
+    case "A":
+        console.log("A -> 9")
+        break;
+    
+    case "B+":
+        console.log("B+ -> 8")
+        break;
+
+    case "B":
+        console.log("B -> 7")
+        break;
+
+    case "C+":
+        console.log("C+ -> 6")
+        break;
+
+    case "C":
+        console.log("C -> 5")
+        break;
+
+    case "D+":
+        console.log("D+ -> 4")
+        break;
+
+    case "D":
+        console.log("D -> 3")
+        break;
+
+    case "E+":
+        console.log("E+ -> 2")
+        break;
+
+    case "E":
+        console.log("E -> 1")
+        break;
+
+    case "F":
+        console.log("F -> 0")
+        break;
+
+    default:
+        console.log("Resposta Invalida!")
+}

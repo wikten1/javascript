3dfb6b66f082c4cc122dff37685fbfb728e9d1cb
wikten_Alves <wiktenalves@gmail.com>
2023-02-07 18:45:57 -0300

    Ex com função 1

---
+++ funcoes/com_funcao_1.js
@@ -0,0 +1,23 @@
+const d1 = 3;
+const m1 = 8;
+const a1 = 2023;
+
+function exibirData1() {
+    console.log(`${d1}/${m1}/${a1}`)
+}
+
+const d2 = 23;
+const m2 = 12;
+const a2 = 2023;
+
+function exibirData2() {
+    console.log(`${d2}-${m2}-${a2}`)
+}
+
+exibirData1();
+exibirData1();
+exibirData1();
+
+exibirData2();
+exibirData2();
+exibirData2();

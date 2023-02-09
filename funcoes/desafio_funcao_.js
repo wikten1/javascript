4005a23d7401bd89eafb901732b96a888a93178d
wikten_Alves <wiktenalves@gmail.com>
2023-02-09 15:05:42 -0300

    desafio função feito

---
+++ funcoes/desafio_funcao_.js
@@ -0,0 +1,14 @@
+
+function somar(V1, V2) {
+   console.log(V1 + V2);
+}
+
+somar(3, 4);
+somar(31, 98);
+
+function subtrair(V1, V2) {
+    console.log(V1 - V2);
+ }
+
+ subtrair (11, 7);
+ subtrair (19, 52);

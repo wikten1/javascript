94b48c0d90aa27f69b6b3b00bc0906b7942040ec
Wikten Alves <wiktenalves@gmail.com>
2024-07-31 15:24:07 -0300

    Create difiudsof.js

---
+++ origamid/funcoes/difiudsof.js
@@ -0,0 +1,10 @@
+var quadrado = {
+    lados: 4,
+    area: function(lado) {
+        return lado * lado;
+    },
+    perimetro: function (lado){
+        return this.lados * lado;
+    },
+    
+}

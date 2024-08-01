df9b98347bd6ab85225026f7312535453661f5fd
Wikten Alves <wiktenalves@gmail.com>
2024-08-01 20:03:57 -0300

    HTML e CSS animais fantásticos

---
+++ origamid/dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -0,0 +1,11 @@
+// Retorne o url da página atual utilizando o objeto window
+const urlPag = window.location.href;
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const primeiroAtivo = document.querySelector('.ativo');
+// Retorne a linguagem do navegador
+const linguagemNav = window.navigator.language;
+
+// Retorne a largura da janela 
+const larguraJanela = window.innerWidth

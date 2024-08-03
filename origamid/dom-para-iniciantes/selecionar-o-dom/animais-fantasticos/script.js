a5b693bbac770ffd2f4db300da9007203c19d254
Wikten Alves <wiktenalves@gmail.com>
2024-08-02 21:19:37 -0300

    Classes e atributos

---
+++ origamid/dom-para-iniciantes/selecionar-o-dom/animais-fantasticos/script.js
@@ -0,0 +1,22 @@
+// Retorne no console todas as imagens do site
+const imagens = document.querySelectorAll('img');
+console.log(imagens);
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensEspeciais = document.querySelectorAll('[src^="img/imagem"]');
+console.log(imagensEspeciais);
+
+
+
+// Selecione todos os links internos (onde o href começa com #)
+const linksInternos = document.querySelectorAll('[href^="#"]');
+console.log(linksInternos);
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+const primeiroH2 = document.querySelector('.animais-descricao h2');
+console.log(primeiroH2);
+
+// Selecione o último p do site
+const ultimoP = document.querySelector('p:last-child');
+console.log(ultimoP)
+

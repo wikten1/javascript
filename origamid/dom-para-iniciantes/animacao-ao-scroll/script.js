0c089d141abe9bd23949cdb37781b883ff8a86c2
Wikten Alves <wiktenalves@gmail.com>
2024-08-07 14:06:47 -0300

    Animação ao scroll

--- origamid/dom-para-iniciantes/animacao-ao-scroll/script.js
+++ origamid/dom-para-iniciantes/animacao-ao-scroll/script.js
@@ -67,15 +67,29 @@ function initScrollSuave() {
 }
 initScrollSuave();
 
-const sections = document.querySelectorAll('.js-scroll');
+function initAnimacaoScroll(){
+  const sections = document.querySelectorAll('.js-scroll');
 
-function animaScroll(){
-  sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top;
-    if(section < 0){
-      section.classList.add('ativo');
+  if (sections.length){
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll(){
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if(isSectionVisible){
+          section.classList.add('ativo');
+        }
+      })
     }
-  })
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+
+  }
+
 }
 
-window.addEventListener('scroll', animaScroll);
+
+initAnimacaoScroll();

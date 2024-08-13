13ec31be40134379151460ff328b634827d1f165
Wikten Alves <wiktenalves@gmail.com>
2024-08-13 15:46:44 -0300

    prototype

---
+++ origamid/objetos/prototype/script.js
@@ -0,0 +1,39 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+function Pessoa (nome, sobrenome, idade){
+   this.nome = nome;
+   this.sobrenome = sobrenome;
+   this.idade = idade;
+}
+
+Pessoa.prototype.nomeCompleta = function() {
+  return `${this.nome} ${this.sobrenome}`
+}
+
+const wikten = new Pessoa('Wikten', 'Alves', 25);
+
+
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
+Object.getOwnPropertyNames(NodeList.prototype)
+Object.getOwnPropertyNames(HTMLAllCollection.prototype)
+Object.getOwnPropertyNames(Document.prototype)
+
+
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li; // HTMLLIElement
+li.click; // Function
+li.innerText; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; // Number
+li.click(); // undefined
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; // String
